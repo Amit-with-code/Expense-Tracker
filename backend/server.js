b490b0f9d0f2b3dc7fc8e7cb76bad3d5d7ee8e76
Amit-with-code <amitkumar2nov2001@gmail.com>
2025-11-25 21:15:16 +0530

    setup the mongodb Atlas in system and some importent code

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import connectDB from './config/mongoDB.js'
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+connectDB();
+
+app.use(express.json())
+app.use(cors())  
+
+
+
+app.listen(port,()=>{
+    console.log(`sever is connected to port : ${port}`)
+})
