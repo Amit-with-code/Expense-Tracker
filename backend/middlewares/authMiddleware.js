9bc17212e3e5f4922f1f0e4a6e83fe440511fdf4
Amit-with-code <amitkumar2nov2001@gmail.com>
2025-12-12 12:59:04 +0530

    adding an authentization

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken"
+import { Types } from "mongoose"
+
+const authMiddle = (req,res,next) => {
+    const token = req.headers.authorization?.split(' ')[1]
+    if (token) {
+        return res.status(401).json({message:"Unathorized"})
+    }
+
+    try {
+        const decoded = jwt.verify( token, process.env.JWT_SECRET)
+
+        if ( !Types.ObjectId.isValid(decoded.id)){
+            return res.status(401).json({message:"Unauthorized:Invalid user"})
+        }
+
+        req.user = { id: decoded.id }
+
+        next()
+
+    } catch (error) {
+        return res.status(401).json({message:"Unathorized"})
+    }
+}
+
+export default authMiddle
